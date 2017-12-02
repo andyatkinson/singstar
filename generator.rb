aaa8bcdbaab9b986c424345300e8fc0e7045601d
Andy Atkinson <andyatkinson@gmail.com>
2017-12-02 13:55:57 -0600

    Updated content and markup

--- generator.rb
+++ generator.rb
@@ -6,7 +6,8 @@ require 'csv'
 rows = []
 
 CSV.foreach('songs.csv') do |row|
-  rows << %{<tr>
+  rows << %{
+  <tr>
     <td>#{row[0]}</td>
     <td>#{row[1]}</td>
     <td>#{row[2]}</td>
@@ -14,5 +15,38 @@ CSV.foreach('songs.csv') do |row|
   }
 end
 
-html = %{<html><body><table>#{rows.join}</table></body></html>}
+html = %{
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Singstar Song List</title>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <table class='table'>
+      <thead>
+        <tr>
+          <th>Disc</th>
+          <th>Artist</th>
+          <th>Song</th>
+        </tr>
+      </thead>
+      <tbody>
+        #{rows.join}
+      </tbody>
+    </table>
+  </body>
+</html>
+}
 File.open('songs.html', 'w'){ |f| f.write(html) }
